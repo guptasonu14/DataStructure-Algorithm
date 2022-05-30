81db0600e48e2c0d96f742fd35faf692cefd6e06
guptasonu14 <sguptabudha@gmail.com>
2022-05-30 09:32:16 +0530

    first commit

---
+++ Mathematics/countDigit.java
@@ -0,0 +1,24 @@
+
+
+public class countDigit {
+    static int countDigits(int x){
+        int res = 0;
+        while(x > 0)
+		{
+				x = x / 10;
+
+				res++;
+		}	
+
+		return res;
+    }
+
+    public static void main(String[] args) {
+        int number = 789;
+
+		System.out.println(countDigits(number));
+
+        
+    }
+    
+}
