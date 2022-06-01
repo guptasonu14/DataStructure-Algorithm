6b00635ebf7b79acb8e26dbf646d622c03137d52
guptasonu14 <sguptabudha@gmail.com>
2022-06-01 09:58:48 +0530

    find number of zero in factorial

---
+++ Mathematics/TrailingZeros.java
@@ -0,0 +1,16 @@
+public class TrailingZeros {
+    static int countZero(int n){
+        int res = 0;
+        for(int i=5;i<n;i=i*5){
+            res =res+(n/i);
+        }
+        return res;
+    }
+
+ public static void main(String[] args) {
+     int number = 251;
+     System.out.println(countZero(number));
+        
+    }
+    
+}
