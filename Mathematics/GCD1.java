83d0b31a9504d16040f288a965ddaffadeda4482
guptasonu14 <sguptabudha@gmail.com>
2022-06-02 08:54:34 +0530

    greatest common divisior by Euclidean method

---
+++ Mathematics/GCD1.java
@@ -0,0 +1,21 @@
+//Euclidean algorithm code
+
+
+public class GCD1 {
+    static int gcd(int a,int b){
+        while( a!=b){
+            if(a>b)
+            a=a-b;
+            else
+            b = b-a;
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        int a=12,b=15;
+        System.out.println(gcd(a,b));
+        
+    }
+    
+}
