820d4c7cd06fab4378b49a12efe66243c4b2bc01
guptasonu14 <sguptabudha@gmail.com>
2022-06-03 10:25:19 +0530

    computing power

---
+++ Mathematics/computingPower.java
@@ -0,0 +1,24 @@
+public class computingPower {
+
+	static int power(int x, int n)
+	{
+		if(n == 0)
+			return 1;
+
+		int temp = power(x, n/2);
+
+		temp = temp * temp;
+
+		if(n % 2 == 0)
+			return temp;
+		else
+			return temp * x; 
+	}
+    public static void main(String[] args) {
+        int x = 3, n = 5;
+
+		System.out.println(power(x, n));
+        
+    }
+    
+}
