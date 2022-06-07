dbd78e4d531aacf4189a867e17badc009619db7a
guptasonu14 <sguptabudha@gmail.com>
2022-06-07 10:03:14 +0530

    right shift

---
+++ BitOperator/RightShift.java
@@ -0,0 +1,10 @@
+public class RightShift {
+    public static void main(String[] args) {
+        int x = 33;
+	    
+	    System.out.println(x >> 2);
+        
+	    System.out.println(x >> 1);
+    }
+    
+}
