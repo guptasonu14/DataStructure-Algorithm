841f88422d53c29778e9ac1ed32f5610105f7c3e
guptasonu14 <sguptabudha@gmail.com>
2022-06-07 09:36:45 +0530

    left shift with neg num

---
+++ BitOperator/LeftShiftwithNegNUm.java
@@ -0,0 +1,11 @@
+public class LeftShiftwithNegNUm {
+    public static void main(String[] args) {
+        int x = 1;
+        System.out.println(~x);
+
+        int y = -1;
+        System.out.println(y<<1);
+        
+    }
+    
+}
