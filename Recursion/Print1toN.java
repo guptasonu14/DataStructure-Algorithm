f18236b08c84a07206632605e6e1bb36c8923892
guptasonu14 <sguptabudha@gmail.com>
2022-06-14 09:26:27 +0530

    print num 1 to N

---
+++ Recursion/Print1toN.java
@@ -0,0 +1,18 @@
+public class Print1toN {
+    static void printToN(int n){
+        if(n == 0)
+        return;
+    
+    printToN(n - 1);
+
+    System.out.print(n+" ");
+
+    }
+    public static void main(String[] args) {
+        int n = 4;
+
+    	printToN(n);
+        
+    }
+    
+}
